34cb17942f8ce47013df69576bc36894df24686e
renancoradini@mail.com <renancoradini@mail.com>
2023-10-17 15:38:07 +0100

    add applogin python

---
+++ app_login_python/main.py
@@ -0,0 +1,30 @@
+## Run selenium and chrome driver to scrape data from cloudbytes.dev
+import time
+import os.path
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+## Setup chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless") # Ensure GUI is off
+chrome_options.add_argument("--no-sandbox")
+
+# Set path to chromedriver as per your configuration
+homedir = os.path.expanduser("~")
+webdriver_service = Service("./driver/chromedriver")
+
+# Choose Chrome Browser
+browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+
+# Get page
+browser.get("https://cloudbytes.dev")
+
+# Extract description from page and print
+description = browser.find_element(By.NAME, "description").get_attribute("content")
+print(description)
+
+#Wait for 10 seconds
+time.sleep(10)
+browser.quit()
