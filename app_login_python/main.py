7ea822b2b180f27c3e2ad299dd82e3455a504843
renancoradini <renancoradini@gmail.com>
2023-10-17 19:50:54 +0100

    teste

--- app_login_python/main.py
+++ app_login_python/main.py
@@ -1,60 +1,26 @@
-## Run selenium and chrome driver to scrape data from cloudbytes.dev
 import time
 import os.path
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+import mypass  # criar mypass.py e atribuir as duas variaveis username="test" e userpass="test"
 
-## Setup chrome options
-chrome_options = Options()
-chrome_options.add_argument("--headless") # Ensure GUI is off
-chrome_options.add_argument("--no-sandbox")
-
-# Set path to chromedriver as per your configuration
-homedir = os.path.expanduser("~")
-webdriver_service = Service("./driver/chromedriver")
+service = Service()
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=options)
 
 
+driver.get("https://learn.acloud.guru/")
+# ...
+time.sleep(5)
 
-# Choose Chrome Browser
-browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
-
-usermail = "aws@alvaromenezes.com.br"
-userpass = "abCD1234"
-
-# Get page
-
-# head to github login page
-browser.get('https://learn.acloud.guru/')
-
-time.sleep(10)
-browser.find_element(By.NAME,"email").send_keys(usermail)
+# driver.find_element(By.NAME, "auth0-lock-social-button-text").click
+driver.find_element(By.NAME, "email").send_keys(mypass.username)
+time.sleep(1)
+driver.find_element(By.NAME, "password").send_keys(mypass.userpass)
 time.sleep(2)
-browser.find_element(By.NAME,"password").send_keys(userpass)
-time.sleep(2)
-browser.find_element(By.NAME,"submit").click
+driver.find_element(By.CLASS_NAME, "auth0-lock-social-button-text").click()
 
-# browser.find_element(By.ID,"1-password").send_keys("abCD1234")
-# browser.find_element(By.ID,"1-submit").click()
-
-
-
-# # find username/email field and send the username itself to the input field
-# browser.find_element("id", "login_field").send_keys(username)
-# # find password input field and insert password as well
-# browser.find_element("id", "password").send_keys(password)
-# # click login button
-# browser.find_element("name", "commit").click()
-
-# browser.find_element
-
-# alt-image-w-arrow
-
-# # Extract description from page and print
-# description = browser.find_element(By.NAME, "description").get_attribute("content")
-# print(description)
-
-#Wait for 10 seconds
-time.sleep(10)
-browser.quit()
+time.sleep(5300)
+driver.quit()
