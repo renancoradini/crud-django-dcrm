e699b77e447b7bbf10701190d2d4fc05d6a03e59
renancoradini@mail.com <renancoradini@mail.com>
2023-10-18 11:30:15 +0100

    add applogin python

--- app_login_python/main.py
+++ app_login_python/main.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+import mypass
 
 service = Service()
 options = webdriver.ChromeOptions()
